74d98787e1751a68bff6ea5cd1988414a2ef19d3
Svein Wiik <sveinwiik@gmail.com>
2024-11-06 22:37:29 +0100

    added flask login

--- app.py
+++ app.py
@@ -1,18 +1,89 @@
-from flask import Flask
-
-app = Flask(__name__)
+# from flask import Flask
+import flask
+import flask_login
 
 from werkzeug.middleware.proxy_fix import ProxyFix
+
+app = flask.Flask(__name__)
+app.secret_key = 'supersecret'
+
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
 
 app.wsgi_app = ProxyFix(
     app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
 )
+
+users = {'foo@bar.tld': {'password': 'secret'}}
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    if email in users and flask.request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized', 401
+
 
 @app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/")
+@flask_login.login_required
 def advokaut():
     return "<p>Advokaut.no rules</p>"
 
